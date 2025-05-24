2095d5bd149aff157dcca9e57e9c99bab9fa817a
anthony.ryan@regionwebllc.com <anthony.chalabis01@gmail.com>
2025-05-24 00:04:15 -0500

    Debugging

--- turner/settings.py
+++ turner/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
